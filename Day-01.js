b7b5e98a5005e0484b176bf50dc2717a247691bf
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-06-11 18:34:19 +0530

    Create Day-01.js

---
+++ Day-01.js
@@ -0,0 +1,11 @@
+var arr = [23,24,25,"guvi"]
+// Array Index always starts with 0
+//How to access the array elements
+// Syntax: var element = arrayname[indexnumber]
+console.log(arr[1]);
+//Array length;
+// Syntax: arrayname.length
+console.log(arr.length);
+for(var i=0;i<arr.length;i++){
+console.log(arr[i]);
+}
