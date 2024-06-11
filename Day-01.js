987c4181a8ee2a779939f9a1f09611b9c9ec9f02
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-06-11 19:06:17 +0530

    Update Day-01.js

--- Day-01.js
+++ Day-01.js
@@ -9,3 +9,8 @@ console.log(arr.length);
 for(var i=0;i<arr.length;i++){
 console.log(arr[i]);
 }
+
+// Conversion of string to array
+var str = "guvi geek";
+var ajees = str.split(" ");
+console.log(ajees)
